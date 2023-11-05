ad62c4a2b212c14df43daebe06e90757cc72476f
mustafatalha222 <mustafatalha222@gmail.com>
2023-11-05 15:55:09 +0500

    counter app completed

---
+++ src/components/Counter.tsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    if (count > 0) setCount(count - 1);
+  };
+
+  const reset = () => {
+    setCount(0);
+  };
+
+  return (
+    <div className="counter">
+      <h1>Counter App</h1>
+      <h1> {count}</h1>
+      <button onClick={decrement} disabled={!count}>
+        Decrement
+      </button>
+      <button onClick={reset}>Reset</button>
+      <button onClick={increment}>Increment</button>
+    </div>
+  );
+};
+
+export default Counter;
