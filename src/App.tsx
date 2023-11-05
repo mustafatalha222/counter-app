e5221c62b4fa5dbd954bc8021ed597596669454a
mustafatalha222 <mustafatalha222@gmail.com>
2023-11-05 18:26:41 +0500

    pages /counter-app show App component

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,34 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Counter from "./components/Counter";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <Counter />
-    </>
+    <Router>
+      <Routes>
+        <Route
+          path="*"
+          element={
+            <>
+              <div>
+                <a href="https://vitejs.dev" target="_blank">
+                  <img src={viteLogo} className="logo" alt="Vite logo" />
+                </a>
+                <a href="https://react.dev" target="_blank">
+                  <img
+                    src={reactLogo}
+                    className="logo react"
+                    alt="React logo"
+                  />
+                </a>
+              </div>
+              <Counter />
+            </>
+          }
+        ></Route>
+      </Routes>
+    </Router>
   );
 }
 
